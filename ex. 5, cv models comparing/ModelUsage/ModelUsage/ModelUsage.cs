69bd87bfc1f30a1ae34daeeaa906677930163e57
SmirnovAlexander <32129186+SmirnovAlexander@users.noreply.github.com>
2019-07-15 15:47:24 +0300

    Added dataset and models

---
+++ ex. 5, cv models comparing/ModelUsage/ModelUsage/ModelUsage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Tensorflow;
+
+namespace ModelUsage
+{
+    class ModelUsage
+    {
+        static void Main(string[] args)
+        {
+            
+
+            using (var sess = tf.Session())
+            {
+                var saver = tf.train.Saver();
+                saver.save(sess, "ensorflowModel.ckpt");
+            }
+        }
+    }
+}
